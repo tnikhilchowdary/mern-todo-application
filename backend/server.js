e335dc00df3e58dd7aee9ff436d3ba63254199ee
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-12-10 16:30:33 -0600

    Server.js file implmented

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import connect from "./database/connectDb.js";
 
-dotenv.config()
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 connect();
+
+app.get("/", (req, res) => {
+    res.send("NodeJs Running");
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on local:${process.env.PORT}`);
+})
